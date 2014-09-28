7107f96e793f8c4d7ccb8af1be243a992e90961c
Omar Faruk <munna93_bd@yahoo.com>
2014-09-29 04:53:38 +0600

    Create headers.h

---
+++ teachBangla/headers.h
@@ -0,0 +1,11 @@
+#include <iGraphics.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
